617e8e497f34197a5e1c101661cd07de748c7646
Sergey <50015502+Redgenal@users.noreply.github.com>
2022-08-29 18:24:31 +0300

    v0.5 grid v

--- script.js
+++ script.js
@@ -1,46 +1,42 @@
-function main (arr) {
-    let min;
-    let col;
-    
-        minId = findMin(document);
-        console.log("min " + minId)
-        col = document.getElementById(minId);
-        let img = document.createElement("img");
-        img.src = arr.url;
-        img.title = arr.title;
-        // img.height = arr.height;
-        col.append(img);
-}
-
-async function getData () {
+(async function getData () {
     let response3 = await fetch('https://api.jsonbin.io/v3/b/6303bf5ea1610e63860a5cf7?meta=false');
     let jsoned = await response3.json();
-    let i = -1;
     console.log(jsoned[0]);
     
-    while (jsoned[++i]) {
-        main(jsoned[i]);
-    }
+    main_work(jsoned, resizeMasonryItem);
+})();
+
+function main_work (arr, callback) {
+  const container = document.querySelector('.masonry');
+
+  for (let i = 0; i < arr.length; i++) {
+      let img = document.createElement("img");
+      img.classList.add("myImg");
+      img.src = arr[i].url;
+      img.title = arr[i].title;
+      img.onload = () => callback(img);
+      container.appendChild(img); 
+  }
 }
 
-getData();
-
-//Смотреть текущую длинну столбца и пушить элементы по одному в самый короткий
-function findMin (document) {
-    let col1 = document.getElementById("col1").clientHeight;
-    let col2 = document.getElementById("col2").clientHeight;
-    let col3 = document.getElementById("col3").clientHeight;
-    let col4 = document.getElementById("col4").clientHeight;
-    let min = Math.min(col1, col2, col3, col4);
-    console.log(col1 + " " + col2 + " " + col3 + " " + col4);
-    switch(min) {
-        case col1:
-            return ("col1");
-        case col2:
-            return ("col2");
-        case col3:
-            return ("col3");
-        default:
-            return ("col4");
-    }
+function resizeMasonryItem(item){
+  let grid = document.querySelector('.masonry');
+  let rowGap = parseInt(getComputedStyle(grid)['grid-row-gap']);
+  let rowHeight = parseInt(getComputedStyle(grid)['grid-auto-rows']);
+  let rowSpan = Math.ceil((item.getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+  
+  item.style.gridRowEnd = 'span ' + rowSpan;
 }
+  
+function resizeImages() {
+  let allItems = document.getElementsByClassName('myImg');
+  
+  for(var i=0;i<allItems.length;i++){
+    resizeMasonryItem(allItems[i]);
+  }
+}
+
+let masonryEvents = ['load', 'resize'];
+masonryEvents.forEach( function(event) {
+  window.addEventListener(event, resizeImages);
+} );
